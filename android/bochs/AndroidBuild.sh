e146b0c87e4101cc96f8dc5121e3251225b1850e
lubomyr <lubomyr31@gmail.com>
2015-01-25 20:38:21 +0200

    first commit

---
+++ android/bochs/AndroidBuild.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+
+LOCAL_PATH=`dirname $0`
+LOCAL_PATH=`cd $LOCAL_PATH && pwd`
+
+ln -sf libsdl-1.2.so $LOCAL_PATH/../../../obj/local/armeabi-v7a/libSDL.so
+ln -sf libsdl-1.2.so $LOCAL_PATH/../../../obj/local/armeabi-v7a/libpthread.so
+ln -sf libsdl_image.so $LOCAL_PATH/../../../obj/local/armeabi-v7a/libSDL_image.so
+ln -sf libsdl_ttf.so $LOCAL_PATH/../../../obj/local/armeabi-v7a/libSDL_ttf.so
+
+
+if [ "$1" = armeabi-v7a ]; then
+if [ \! -f bochs/configure ] ; then
+	sh -c "cd bochs && ./bootstrap.sh"
+fi
+
+if [ \! -f bochs/Makefile ] ; then
+env CFLAGS="-Ofast -ffast-math" \
+	../setEnvironment-armeabi-v7a.sh sh -c "cd bochs && ./configure --build=x86_64-unknown-linux-gnu --host=arm-linux-androideabi --with-sdl"
+fi
+
+make -C bochs && mv -f bochs/bochs libapplication-armeabi-v7a.so
+fi
