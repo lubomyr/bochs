5d78a9f0e28b0d224003e89351b11dc543038661
lubomyr <lubomyr31@gmail.com>
2016-02-07 19:55:33 +0200

    Improved AndroidBuild.sh scripts & AndroidAppSettings.cfg

--- android/bochs/AndroidBuild.sh
+++ android/bochs/AndroidBuild.sh
@@ -10,16 +10,16 @@ ln -sf libsdl_image.so $LOCAL_PATH/../../../obj/local/$1/libSDL_image.so
 ln -sf libsdl_ttf.so $LOCAL_PATH/../../../obj/local/$1/libSDL_ttf.so
 
 
-if [ \! -f bochs-$1/configure ] ; then
-	sh -c "cd bochs-$1 && ./bootstrap.sh"
+if [ \! -f bochs/configure ] ; then
+	sh -c "cd bochs && ./bootstrap.sh"
 fi
 
-if [ \! -f bochs-$1/Makefile ] ; then
+if [ \! -f bochs/Makefile ] ; then
 
 env CFLAGS="-Ofast -ffast-math" \ 
 env LIBS="-lgnustl_static" \
-	../setEnvironment-$1.sh sh -c "cd bochs-$1 && ./configure --build=x86_64-unknown-linux-gnu --host=$2 --with-sdl --enable-all-optimizations --enable-clgd54xx --enable-voodoo --enable-sb16 --disable-gameport --enable-ne2000 --enable-usb"
+	../setEnvironment-$1.sh sh -c "cd bochs && ./configure --build=x86_64-unknown-linux-gnu --host=$2 --with-sdl --enable-all-optimizations --enable-clgd54xx --enable-voodoo --enable-sb16 --disable-gameport --enable-ne2000 --enable-usb"
 fi
 
-make -C bochs-$1 && mv -f bochs-$1/bochs libapplication-$1.so
+make -C bochs && mv -f bochs/bochs libapplication-$1.so
 
